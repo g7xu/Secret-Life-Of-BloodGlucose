c08024bbed459294d6558f16ef2e882b4950622b
unknown <linybao1@gmail.com>
2025-03-07 06:42:19 -0800

    v1

--- script.js
+++ script.js
@@ -44,4 +44,22 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   });
 
+  let $path = document.querySelector(".mat"),
+    $logo = document.querySelector(".bowl"),
+    $fork = document.querySelector(".fork"),
+    $knife = document.querySelector(".knife");
+
+let tl = gsap.timeline({ repeat: 1, repeatDelay: 0.5 }); 
+
+const start = "M 0 100 V 50 Q 50 0 100 50 V 100 z";
+const end = "M 0 100 V 0 Q 50 0 100 0 V 100 z";
+
+tl.to($path, { duration: 0.8, attr: { d: start }, ease: "power2.in" })
+  .to($path, { duration: 0.4, attr: { d: end }, ease: "power2.out" })
+  .from($logo, { duration: 0.8, y: 75 }, "-=0.8")  // ✅ Moves bowl first
+  .from([$fork, $knife], { duration: 0.8, y: 75, stagger: 0 }, "-=0.3"); 
+
+// Start animation
+tl.play();
+
 });
